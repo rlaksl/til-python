23e6cb81e5f2e44f76b157b07e4c6e9f91c4a9aa
rlaksl <eehiuh@gmail.com>
2025-09-29 20:33:49 +0900

    Day 01 - Python 기초 학습 완료

---
+++ day01/start.py
@@ -0,0 +1,21 @@
+print("Hello world!")
+
+# 숫자 연산 결과 저장
+a = 2
+b = 3
+c = a + b
+print(c)
+
+# 숫자 저장
+age = 88
+
+# 문자열 저장
+my_name = "hwanhui"
+print(my_name)
+
+# boolean
+dead = False
+
+# 마무리
+print("Hello my name is", my_name)
+print("and I'm", age, "years old")
